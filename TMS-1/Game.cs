5b4754780b914a88118e058a7b6f986ca2957c7e
Lizaveta Buglak <lizabuglak@MacBook-Air-Lizaveta.local>
2026-07-30 17:13:09 +0300

    Complete homework 2

--- TMS-1/Game.cs
+++ TMS-1/Game.cs
@@ -8,41 +8,52 @@ class Game
         RoundsToPlay = roundsToPlay;
     }
     
-    private int _roundsPlayed = 0; // сохранить состояние можно в полях
+    private int _roundsPlayed = 0;
+    private int _playerScore = 0;
+    private int _computerScore = 0;
 
-    public int RoundsToPlay { get; } // авто-свойство (приватное поле и простой геттер + сеттер)
+    public int RoundsToPlay { get; private set; }
 
     public bool UserWon { get; private set; }
 
-    public string DisplayRoundsPlayed(bool someParam, int roundsPlayed = 2, int someAdditionalParam = 3) => $"Game has {roundsPlayed} rounds played";
+    public string DisplayRoundsPlayed(bool someParam, int roundsPlayed = 2, int someAdditionalParam = 3) =>
+        $"Game has {roundsPlayed} rounds played";
 
     public void Play()
     {
         Console.WriteLine("Hello this is Rock Paper Scissors");
+
+        int rounds;
+
+        do
+        {
+            Console.WriteLine("Enter number of rounds:");
+        } 
+        while (!int.TryParse(Console.ReadLine(), out rounds) || rounds <= 0);
+
+        RoundsToPlay = rounds;
+
         Console.WriteLine("Enter your step");
 
         UserWon = false;
 
         do
         {
+            Console.WriteLine();
+            Console.WriteLine($"Round {_roundsPlayed + 1}/{RoundsToPlay}");
+
             Console.WriteLine("1 - Rock");
             Console.WriteLine("2 - Paper");
             Console.WriteLine("3 - Scissors");
+            Console.WriteLine("4 - Well");
             Console.WriteLine("0 - Exit");
 
-            _roundsPlayed++;
+            var userInput = Console.ReadLine();
 
-            var userInput = Console.ReadLine(); // "5"
+            int userChoice;
 
-            int userChoice; // there is no value
-
-            // parse user input = string into int 
-            // put result to out result param
-            // return if parse was successful
-
-            if (!int.TryParse(userInput, out userChoice) || !(userChoice >= 0 && userChoice <= 3))
+            if (!int.TryParse(userInput, out userChoice) || !(userChoice >= 0 && userChoice <= 4))
             {
-                Console.WriteLine(userChoice);
                 Console.WriteLine("Invalid input");
                 continue;
             }
@@ -51,31 +62,76 @@ class Game
             {
                 return;
             }
-            
+
+            _roundsPlayed++;
+
             string userChoiceString = CalculateMoveName(userChoice);
             Console.WriteLine($"You chose {userChoiceString}");
-            
+
             var random = new Random();
-            var computerChoice = random.Next(1, 4); // generate random number 1-3
+            var computerChoice = random.Next(1, 5);
 
             string computerChoiceString = CalculateMoveName(computerChoice);
-            
-            //CalculateMoveName()
 
             Console.WriteLine($"Computer chose {computerChoiceString}");
 
             var winnerExists = TryCalculateWinner(userChoice, computerChoice, out var winner);
+
             if (winnerExists)
             {
                 Console.WriteLine($"{winner} won the round");
+
+                if (winner == "Player 1")
+                {
+                    _playerScore++;
+                }
+                else
+                {
+                    _computerScore++;
+                }
             }
             else
             {
                 Console.WriteLine("It's a draw");
             }
-            
+
+            Console.WriteLine($"Score: Player {_playerScore} : {_computerScore} Computer");
+
+
+            // Проверка досрочной победы
+            int remainingRounds = RoundsToPlay - _roundsPlayed;
+
+            if (_playerScore > _computerScore + remainingRounds)
+            {
+                Console.WriteLine("Player wins the game!");
+                return;
+            }
+
+            if (_computerScore > _playerScore + remainingRounds)
+            {
+                Console.WriteLine("Computer wins the game!");
+                return;
+            }
+
         } while (_roundsPlayed < RoundsToPlay);
+
+
+        Console.WriteLine();
+
+        if (_playerScore > _computerScore)
+        {
+            Console.WriteLine("Player wins the game!");
+        }
+        else if (_computerScore > _playerScore)
+        {
+            Console.WriteLine("Computer wins the game!");
+        }
+        else
+        {
+            Console.WriteLine("Draw");
+        }
     }
+
 
     private string CalculateMoveName(int moveNumber)
     {
@@ -85,21 +141,33 @@ class Game
                 return "Rock";
             case 2:
                 return "Paper";
-            default:
+            case 3:
                 return "Scissors";
+            case 4:
+                return "Well";
+            default:
+                return "";
         }
     }
+
 
     private bool TryCalculateWinner(int player1Move, int player2Move, out string winner)
     {
         if (player1Move == player2Move)
         {
             winner = "";
-            return false; 
+            return false;
         }
 
-        if (player1Move == 1 && player2Move == 3 || player1Move == 2 && player2Move == 1 ||
-            player1Move == 3 && player2Move == 2)
+
+        if (
+            (player1Move == 1 && player2Move == 3) || // Rock beats Scissors
+            (player1Move == 2 && player2Move == 1) || // Paper beats Rock
+            (player1Move == 3 && player2Move == 2) || // Scissors beats Paper
+            (player1Move == 2 && player2Move == 4) || // Paper beats Well
+            (player1Move == 4 && player2Move == 1) || // Well beats Rock
+            (player1Move == 4 && player2Move == 3)    // Well beats Scissors
+        )
         {
             winner = "Player 1";
         }
